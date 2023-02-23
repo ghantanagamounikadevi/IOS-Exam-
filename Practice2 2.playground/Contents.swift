d5b4b5bd882d1a91e9ad5d56cc936f5909ed8c54
ghantanagamounikadevi <111657402+ghantanagamounikadevi@users.noreply.github.com>
2023-02-23 15:10:07 -0600

    Strings2

---
+++ Practice2 2.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+
+var mobileBrand = "Apple"
+mobileBrand = "Samsung"
+print(mobileBrand)
+     
+let pi = 3.14
+print(pi)
+//Explicit Declaration of Variable
+
+var age : Int = 23
+age = age * 2
+print(age)
+
+var aweMessage = "This is Superb!"
+print(aweMessage)
+print("aweMessage")
+
+var course1 = "iOS"
+var course2 = "Java"
+print(course1,course2)
+print(course1,"-",course2)
+
+print(10,20,30)
+    print(12.5,15.5)
+
+
+var cricketKit = ("handgloves","helmet",("bat","ball"))
+print(cricketKit.0)
+print(cricketKit.1)
+print(cricketKit.2.0)
+print(cricketKit.2.1)
+print(type(of: cricketKit))
+    
