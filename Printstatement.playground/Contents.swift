936fe9ecb05fb0d9064cb106c6c2562ffe15cc08
ghantanagamounikadevi <111657402+ghantanagamounikadevi@users.noreply.github.com>
2023-02-23 15:04:21 -0600

    Strings

---
+++ Printstatement.playground/Contents.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("Hi",10,10.23)
+var name = "Mounika"
+print("Hello,\(name)!")
+print("Hello "+name+"!")
+
+var i=10
+//print("This is ten"+i)   Get Error bcoz cancatinating string //with number
+print("""
+Hello, World!
+From Maryville,
+MO.
+🙂
+Alright
+""")
+
+var age = 23
+print("You are \(age) Years old and in another \(age) years, you will be \(age * 2)")
+print(age-10)
+age = age+10
+print(age)
+print(33*2)
+
+
+print("Hello, \rWelcome to swift programming")
+
+print("I am going to print",price,gst)
+
+var price = 10.25
+var gst = 0.25
+print(price)
+print("Total price \(price+gst)")
+
+print("""
+Hello
+Your total
+Amount
+is
+""")
+
+print("Welcome, Hello all \r Heartly welcome back")
+
+let PI = 3.14
+print(PI-2)
+//PI = PI-1//get error bcoz PI mention as a constant
+
+//var p = 99.9
+var p:Double = 99.9
+//var k = -28 //If not mention data type it is implicit
+var k:Int = -28 //If we mention datatype it is explicit
+
+let message : String = "Hello!"
+print(message,"All")
+
+//message = "Good Bye" // Get error
+
+print("Welcome to swift programming")
+print("Fall 2021")
+print("*******")
+print("Welcome to swift programming" , terminator : "-")
+print("Fall 2021")
+
+
+print(10,20,30,40, separator: "#")
+      
+
+
+
+
+
+
+
+
+
+
